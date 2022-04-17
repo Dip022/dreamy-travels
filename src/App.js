b01840d3a0dc96cb6dfbfb07f8d03d602aaf8ffd
Dipok Roy <roydipok022@gmail.com>
2022-04-18 01:22:53 +0600

    create header footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Footer from './Shared/Footer/Footer';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <Header></Header>
+     <Footer></Footer>
     </div>
   );
 }
