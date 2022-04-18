ea1c72b4d0fda7828c45d5ef23fe4d179c92a340
Dipok Roy <roydipok022@gmail.com>
2022-04-18 12:39:57 +0600

    banner added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import banner from "../../images/banner.jpg";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div className="home-container">
+      <div className="banner">
+        <img src={banner} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
