ac519c3daac824fde4b3916cd60ed17e7bc4adbb
Dipok Roy <roydipok022@gmail.com>
2022-04-19 00:08:55 +0600

    firebase init ignore

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDvRXeUPyjKiQvMeVqQw7_ppiYR5aNuSH8",
-  authDomain: "dreamy-travels-e732d.firebaseapp.com",
-  projectId: "dreamy-travels-e732d",
-  storageBucket: "dreamy-travels-e732d.appspot.com",
-  messagingSenderId: "975101748154",
-  appId: "1:975101748154:web:89c9f87b5814d23f8eb383",
-  measurementId: "G-CVYZ2Y7G4M",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
