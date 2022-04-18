b3d8ca6067d27da8854e0b722e3569322459299d
Dipok Roy <roydipok022@gmail.com>
2022-04-18 12:23:29 +0600

    menubar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
+import About from "./Pages/About/About";
+import Blog from "./Pages/Blog/Blog";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Logins/Login/Login";
+import Registration from "./Pages/Logins/Registration/Registration";
+import Services from "./Pages/Services/Services";
 import Footer from "./Shared/Footer/Footer";
 import Header from "./Shared/Header/Header";
 
@@ -10,6 +15,15 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route
+          path="/registration"
+          element={<Registration></Registration>}
+        ></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,27 +1,47 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <>
-           <Navbar collapseOnSelect expand="lg" sticky="top" bg="primary" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Dreamy Travels</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="ms-auto">
-                        <Nav.Link href="#features">Home</Nav.Link>
-                        <Nav.Link href="#pricing">Services</Nav.Link>
-                        <Nav.Link href="#pricing">About</Nav.Link>
-                        <Nav.Link href="#pricing">Blog</Nav.Link>
-                        <Nav.Link href="#pricing">Login</Nav.Link>
-                        <Nav.Link href="#pricing">Register</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    );
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        bg="primary"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand href="#home">Dreamy Travels</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="services">
+                Services
+              </Nav.Link>
+              <Nav.Link as={Link} to="about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="blog">
+                Blog
+              </Nav.Link>
+              <Nav.Link as={Link} to="login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="registration">
+                Registration
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
